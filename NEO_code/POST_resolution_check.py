9790666c74573691a3ccffa9d09e36bdfc2e00a0
Joey NERSC <joseph.schmidt@utexas.edu>
2023-03-22 20:39:22 -0700

    Resolution check for neo runs

--- NEO_code/POST_resolution_check.py
+++ NEO_code/POST_resolution_check.py
@@ -1,40 +1,8 @@
-import os
 
-print('Test')
-# Check if file exists
-if os.path.isfile('out.neo.run'):
-    
-    # Create empty dictionary
-    info_dict = {}
 
-    # Open file and read lines
-    with open('out.neo.run', 'r') as f:
-        lines = f.readlines()
+from POST_out_neo_run_2_dict import out_neo_data_2_dict
 
-    # Loop through lines
-    for line in lines:
-
-        # Ignore lines that don't contain a colon
-        if ':' not in line:
-            continue
-
-        print(line)
-        # Split line at the colon
-        key, value = line.split(':')
-
-        # Strip whitespace from key and value
-        key = key.strip()
-        value = value.strip()
-
-        # Convert numerical values to floats
-        if value.replace('.', '', 1).isdigit():
-            value = float(value)
-
-        # Add key-value pair to dictionary
-        info_dict[key] = value
-
-    # Print resulting dictionary
-    print(info_dict)
-
-else:
-    print('File not found.')
+info_dict = out_neo_data_2_dict()
+print('post')
+print(info_dict)
+print('yo')
