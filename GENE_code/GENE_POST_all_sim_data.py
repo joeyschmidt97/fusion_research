889af09479b7e0c2b669476fb76190d3eec28d1b
Joey NERSC <joseph.schmidt@utexas.edu>
2023-03-31 12:59:41 -0700

    Made function to write simulation list data

--- GENE_code/GENE_POST_all_sim_data.py
+++ GENE_code/GENE_POST_all_sim_data.py
@@ -1,42 +1,53 @@
 #!/usr/bin/env python3
 
 # This code will go into all the sub-directories and extract all parameters data 
-
 import os
-from GENE_POST_param_2_dict import param_list
+from GENE_POST_param_data import parameters_to_list
+from GENE_POST_omega_data import omegas_to_list
 
 
-def sim_data():
-    simulation_dict = {} 
+def simulations_to_data():
+    simulation_list = []
 
     cwd = os.getcwd()
     directory_list = os.listdir(cwd)
     directory_list.sort()
 
     for directory_name in directory_list:
+        simulation_dict = {} 
         directory_path = os.path.join(cwd, directory_name)
 
-        if os.path.isdir(directory_path):   
-            parameter_list = param_list(directory_path)
-
-
+        if os.path.isdir(directory_path):
+            parameter_list = parameters_to_list(directory_path)
             simulation_dict['parameters'] = parameter_list
+
+            omega_list = omegas_to_list(directory_path)
+            simulation_dict['omegas'] = omega_list
+
+            simulation_dict['filepath'] = directory_path
+            simulation_list.append(simulation_dict)
 
         else:  
             pass
 
-    return simulation_dict
+    return simulation_list
+
+
 
 if __name__ == "__main__":
-    simulation_dict = sim_data()
+    simulation_list = simulations_to_data()
 
-    parameter_list = simulation_dict['parameters']
+    for simulation_dict in simulation_list:
 
-    for parameter in parameter_list:
-        print(parameter['kymin'], parameter['diagdir'])
-    
+        filepath = simulation_dict['filepath']
+        parameter_list = simulation_dict['parameters']
+        omega_list = simulation_dict['omegas']
 
+        print(filepath)
 
+        for parameter in parameter_list:
+            print(parameter['kymin'], parameter['filename'])
 
-
-
+        for omega in omega_list:
+            print(omega['kymin'], omega['gamma (cs/a)'], omega['omega (cs/a)'], omega['filename'])
+        
