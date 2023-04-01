a7765ff8da51059f12c645f36245227a4ca3aa37
Joey NERSC <joseph.schmidt@utexas.edu>
2023-04-01 16:15:56 -0700

    Added specific filepath function activation and command line activation

--- GENE_code/GENE_POST_all_sim_data.py
+++ GENE_code/GENE_POST_all_sim_data.py
@@ -6,39 +6,44 @@ from GENE_POST_param_data import parameters_to_list
 from GENE_POST_omega_data import omegas_to_list
 
 
-def simulations_to_data():
+def simulations_to_data(filepath):
     simulation_list = []
 
-    cwd = os.getcwd()
-    directory_list = os.listdir(cwd)
+    directory_list = os.listdir(filepath)
     directory_list.sort()
 
     for directory_name in directory_list:
-        simulation_dict = {} 
-        directory_path = os.path.join(cwd, directory_name)
-
-        if os.path.isdir(directory_path):
-            parameter_list = parameters_to_list(directory_path)
-            simulation_dict['parameters'] = parameter_list
-
-            omega_list = omegas_to_list(directory_path)
-            simulation_dict['omegas'] = omega_list
-
-            simulation_dict['filepath'] = directory_path
-            simulation_list.append(simulation_dict)
-
-        else:  
+        
+        # Skip files that start with "X_" 
+        if directory_name.startswith('X_'):
             pass
+
+        else:
+            simulation_dict = {} 
+            directory_path = os.path.join(filepath, directory_name)
+
+            if os.path.isdir(directory_path):
+                parameter_list = parameters_to_list(directory_path)
+                simulation_dict['parameters'] = parameter_list
+
+                omega_list = omegas_to_list(directory_path)
+                simulation_dict['omegas'] = omega_list
+
+                simulation_dict['filepath'] = directory_path
+                simulation_list.append(simulation_dict)
+
+            else:  
+                pass
 
     return simulation_list
 
 
 
 if __name__ == "__main__":
-    simulation_list = simulations_to_data()
+    cwd = os.getcwd()
+    simulation_list = simulations_to_data(cwd)
 
     for simulation_dict in simulation_list:
-
         filepath = simulation_dict['filepath']
         parameter_list = simulation_dict['parameters']
         omega_list = simulation_dict['omegas']
